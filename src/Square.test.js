b3c1e8a83ced10f7b01ee5441d82513f67f8f18d
Jennifer Folkerth <jennifer.folkerth@gmail.com>
2018-12-09 16:58:49 -0500

    Alerting on square click

--- src/Square.test.js
+++ src/Square.test.js
@@ -14,3 +14,10 @@ it('displays number', () => {
    const square = shallow(<Square value={4}/>);
    expect(square.text()).toEqual('4');
 });
+
+it('alerts on click', () => {
+    window.alert = jest.fn();
+   const square = shallow(<Square value={3}/>);
+   square.simulate('click');
+   expect(window.alert).toHaveBeenCalledWith('clicked');
+});
